7a503f9dbae159cda687ac9b761019826148ece1
Ali Yousefi <a_yousefi78@yahoo.com>
2021-12-28 19:12:02 +0330

    theme completed, finished

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.clear();
 const { gsap, imagesLoaded } = window;
 
 const buttons = {
-    prev: document.querySelector(".btn--left"),
-    next: document.querySelector(".btn--right"),
+    prev: document.getElementById("arrow-left"),
+    next: document.getElementById("arrow-right"),
 };
 const cardsContainerEl = document.querySelector(".cards__wrapper");
 const appBgContainerEl = document.querySelector(".app__bg");
